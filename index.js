43dcab311b0f5609904ccdac7a7f3d4b5de349cf
Miguel Vilao <mgvilao1@gmail.com>
2020-05-12 13:00:38 +0100

    removed node_modeules and updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // instantiate express and set the app to look for the html and css source files
 // set the app to listen to the server on port 5000
-// set index.html as home
+// set index.html as homepage
 const express = require("express");
 const app = express();
 
